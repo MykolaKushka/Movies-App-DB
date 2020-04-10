f3d37ec11ad3395fd7d38740afe5411890088f7c
Nick <Mykola.Kushka@gmail.com>
2020-04-10 14:49:53 +0300

    JSON updated

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -4,17 +4,25 @@ const MovieItem = props => {
   const { movie, removeMovie } = props;
 
   return (
-    <div className="card-body">
-      <h6 className="card-title">{movie.title}</h6>
-      <div className="d-flex justify-content-between align-items-center">
-        <p className="mb-0">Rating: {movie.vote_average}</p>
-        <button type="button" className="btn btn-secondary">
-          Will watch
+    <div className="card">
+      <img
+        className="card-img-top"
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path ||
+          movie.poster_path}`}
+        alt=""
+      />
+      <div className="card-body">
+        <h6 className="card-title">{movie.title}</h6>
+        <div className="d-flex justify-content-between align-items-center">
+          <p className="mb-0">Rating: {movie.vote_average}</p>
+          <button type="button" className="btn btn-secondary">
+            Will watch
+          </button>
+        </div>
+        <button onClick={removeMovie.bind(null, props.movie)}>
+          Delete movie
         </button>
       </div>
-      <button onClick={removeMovie.bind(null, props.movie)}>
-        Delete movie
-      </button>
     </div>
   );
 };
