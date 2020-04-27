4ef1815236f357679a16fa26a0c343a1aba94136
Nick <Mykola.Kushka@gmail.com>
2020-04-27 15:08:41 +0300

    Pagination added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,8 @@ class App extends React.Component {
     this.state = {
       movies: [],
       moviesWillWatch: [],
-      sort_by: "revenue.desc"
+      sort_by: "revenue.desc",
+      page: 1
     };
   }
 
@@ -22,6 +23,8 @@ class App extends React.Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.sort_by !== this.state.sort_by) {
       this.getMovies();
+    } else if (prevState.page !== this.state.page) {
+      this.getMovies();
     }
   }
 
@@ -29,14 +32,16 @@ class App extends React.Component {
     fetch(
       `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${
         this.state.sort_by
-      }`
+      }&page=${this.state.page}`
     )
       .then(response => {
         return response.json();
       })
       .then(data => {
         this.setState({
-          movies: data.results
+          movies: data.results,
+          allPages: data.total_pages,
+          curPage: data.page
         });
       });
   };
@@ -81,7 +86,33 @@ class App extends React.Component {
     });
   };
 
+  changePage = num => {
+    this.setState({
+      page: this.state.page + num
+    });
+  };
+
   render() {
+    const prevButton = page => {
+      if (page > 1) {
+        return (
+          <button type="button" onClick={() => this.changePage(-1)}>
+            Prev
+          </button>
+        );
+      }
+    };
+
+    const nextButton = page => {
+      if (page < this.state.allPages) {
+        return (
+          <button type="button" onClick={() => this.changePage(1)}>
+            Next
+          </button>
+        );
+      }
+    };
+
     return (
       <div className="container">
         <div className="row">
@@ -111,6 +142,18 @@ class App extends React.Component {
           </div>
           <div className="col-3">
             <p>Will watch: {this.state.moviesWillWatch.length}</p>
+          </div>
+        </div>
+        <div className="row mb-3 text-center">
+          <div className="col-12">
+            {prevButton(this.state.curPage)}
+            <span className="mx-2">Current page: {this.state.curPage}</span>
+            {nextButton(this.state.curPage)}
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-12 text-center">
+            Total pages: {this.state.allPages}
           </div>
         </div>
       </div>
